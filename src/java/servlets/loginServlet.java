32d4bbe6ca389259ef731ca68efccc19578c6732
794980 <794980@SAIT224429ISS.ACDM.DS.SAIT.CA>
2020-11-11 11:20:08 -0700

    Week 5 - Lab 4 Implementation of AccountService Implementation of loginServlet

--- src/java/servlets/loginServlet.java
+++ src/java/servlets/loginServlet.java
@@ -11,76 +11,56 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.AccountService;
+import models.User;
 
 /**
  *
  * @author 794980
  */
 public class loginServlet extends HttpServlet {
+     @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet loginServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet loginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        if (request.getParameter("logout") != null) {
+            session.invalidate();
+            session = request.getSession();
+            request.setAttribute("message", "See you later you have logged out");
+        }
+        if (session.getAttribute("user") != null){
+            response.sendRedirect("home");
+            return;
+        }
+        getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        AccountService account = new AccountService();
+        HttpSession session = request.getSession(false);
+        String userName = request.getParameter("username");
+        String password = request.getParameter("password");
+
+         if (userName != null && password != null) {
+            if (account.login(userName, password) != null) {
+                User user = account.login(userName, password);
+                session.setAttribute("user", user);
+                response.sendRedirect("home");
+            } else {
+                request.setAttribute("username", userName);
+                request.setAttribute("password", password);
+                request.setAttribute("message", "Username or password don't match");
+                
+                getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+            }
+        } else {
+            request.setAttribute("username", userName);
+            request.setAttribute("password", password);            
+            request.setAttribute("message", "Username and password must be filled in");
+            
+            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
